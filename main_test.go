883cb9e974bda7c53bd2e09f44a0e2a9124e5a17
Rassul Yunussov <yunussov@gmail.com>
2023-12-08 13:47:32 +0200

    add test

---
+++ main_test.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"testing"
+
+	"gotest.tools/v3/assert"
+)
+
+func TestServersStarted(t *testing.T) {
+	serversCount := 10
+	for i := 0; i < serversCount; i++ {
+		startHttpServer(i, fmt.Sprintf(":808%d", i), fmt.Sprintf("hello from %d", i))
+	}
+	for i := 0; i < serversCount; i++ {
+		resp, err := http.Get(fmt.Sprintf("http://localhost:808%d", i))
+		assert.NilError(t, err)
+		defer resp.Body.Close()
+		bytes, err := io.ReadAll(resp.Body)
+		assert.NilError(t, err)
+		assert.Equal(t, string(bytes), fmt.Sprintf("hello from %d", i))
+	}
+}
